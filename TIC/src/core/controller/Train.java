d5b0a18cddc9df92035f5e857b45b07ca751e85b
MaximeJoly95 <joly.maxime28@orange.fr>
2018-02-07 10:49:30 +0100

    Add files via upload
    Initialisation of the line done.

---
+++ TIC/src/core/controller/Train.java
@@ -0,0 +1,112 @@
+package core.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import core.Constants;
+
+public class Train extends Thread {
+
+	//List of the different train
+	public final static int SHORT_TYPE = 0;
+	public final static int LONG_TYPE = 1;
+	
+	//Maximum capacity of the Train
+	private int maxPassengers;
+		
+	//Number of current passenger
+	private volatile int currentPassenger;
+	
+	//Canton of the train
+	private volatile Canton currentCanton;
+	
+	//Current position in the Canton
+	private volatile int currentPosition;
+	
+	//Type of the train
+	private int type;
+	
+	//Contain the number of the passenger and the Station where they want to go
+	private Map<Integer,Integer> destination = new HashMap<Integer,Integer>();
+	
+	// Indicate if the train 
+	private boolean hasArrived = false;
+	
+	// Speed of the train
+	private int speed;
+	
+	
+	/**************************************************************************/
+
+	public Train(int currentPassenger, Map<Integer, Integer> destination, int speed, int type) {
+		this.currentPassenger = currentPassenger;
+		this.destination = destination;
+		this.speed = speed;
+		this.type = type;
+	}
+	
+	@Override
+	public void run(){
+		while (!hasArrived){
+			try {
+				sleep(Constants.TIME_UNIT);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			setCurrentPosition(currentPosition++);
+		}
+	}
+
+	public int getMaxPassengers() {
+		return maxPassengers;
+	}
+
+	public int getCurrentPassenger() {
+		return currentPassenger;
+	}
+
+	public Canton getCurrentCanton() {
+		return currentCanton;
+	}
+
+	public int getCurrentPosition() {
+		return currentPosition;
+	}
+
+	public int getType() {
+		return type;
+	}
+	
+	public int getSpeed(){
+		return speed;
+	}
+
+	public Map<Integer, Integer> getDestination() {
+		return destination;
+	}
+
+	public void setCurrentPassenger(int currentPassenger) {
+		this.currentPassenger = currentPassenger;
+	}
+
+	public void setCurrentCanton(Canton currentCanton) {
+		this.currentCanton = currentCanton;
+	}
+
+	public void setCurrentPosition(int currentPosition) {
+		this.currentPosition = currentPosition;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public void setSpeed(int speed){
+		this.speed = speed;
+	}
+
+	public void setDestination(Map<Integer, Integer> destination) {
+		this.destination = destination;
+	}
+		
+}
---
+++ TIC/src/core/dom/XMLParser.java
@@ -0,0 +1,97 @@
+package core.dom;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * XML parser
+ * 
+ * @author Maxime
+ *
+ */
+public class XMLParser {
+	
+	private DocumentBuilderFactory factory;
+	private DocumentBuilder builder;
+	private Document document;
+	
+	/**
+	 * Constructor
+	 * @param file the XML file
+	 */
+	public XMLParser(File file){
+		factory = DocumentBuilderFactory.newInstance();
+		try {
+			builder = factory.newDocumentBuilder();
+			document = builder.parse(file);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Get the count of a node giving a tagname
+	 * @param tagname the name of a node
+	 * @return count of the node
+	 */
+	public int getCount(String tagname){
+		NodeList nodes = document.getElementsByTagName(tagname);
+		return nodes.getLength();
+	}
+	
+	/**************************** APPLICATION FOR THE LINE PROJECT **************************/
+	
+	/**
+	 * Get the datas of a list of same nodes (same names)
+	 * wich contains only text node
+	 * @param tagname the name of the node
+	 * @return a map collection of datas
+	 */
+	public Map<String, Map<String, String>> getListInfos(String tagname){
+		
+		// We get the list of node hat have the tagname specified
+		Map<String, Map<String, String>> result = new HashMap<String, Map<String, String>>();
+		
+		// Normally, there is only one child found with the tagname given
+		NodeList nodes = document.getElementsByTagName(tagname);
+		
+		for (int i = 0 ; i < nodes.getLength() ; i++){
+			
+			// We get the current child
+			Node node = nodes.item(i);
+			
+			// We get the value of the attribute id of the node
+			String id = ((Element) node).getAttribute("id");
+			
+			// We get the children of the current child
+			NodeList children = node.getChildNodes();		
+			
+			// Storage of the datas of child node : key is node name and value is the value of child
+			Map<String, String> data = new HashMap<String, String>(); 
+			
+			for (int j = 0 ; j < children.getLength() ; j++){	
+				Node child = children.item(j);	
+				// We consider that the children are only text nodes
+				data.put(child.getNodeName(), child.getTextContent());
+			}
+			
+			// We add the data in the result map
+			result.put(id, data);
+		}
+		
+		return result;
+	}
+
+}
