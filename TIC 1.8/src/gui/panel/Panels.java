ebf32ddcc3f3ab47a3900aa950e136c7921fb59a
ArnaudSery <ursali78@gmail.com>
2018-02-23 10:56:55 +0100

    Add files via upload
    statistiques dynamique

---
+++ TIC 1.8/src/gui/panel/Panels.java
@@ -0,0 +1,12 @@
+package gui.panel;
+
+public class Panels {
+	
+	// List of panels used
+	public static final String MENU 			= "menu";
+	public static final String INFORMATION 		= "information";
+	public static final String PARAMETERS 		= "parametres";
+	public static final String SIMULATION 		= "simulation";
+	public static final String STATISTICS 		= "statistique";
+
+}
